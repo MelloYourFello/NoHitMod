6ddc2aec9a90b7e59131ac02cefddbd38e047117
MelloYourFello <93459743+MelloYourFello@users.noreply.github.com>
2022-09-05 21:20:30 +1200

    Update ExamplePatch.cs

--- Patches/ExamplePatch.cs
+++ Patches/ExamplePatch.cs
@@ -1,4 +1,4 @@
-﻿using HarmonyLib;
+using HarmonyLib;
 using Lamb.UI;
 using System;
 using System.Collections.Generic;
@@ -10,10 +10,16 @@ namespace NoHitMod.Patches
     [HarmonyPatch]
     internal class PlayerControllerPatch
     {
-        [HarmonyPatch(typeof(PlayerController), methodName:"OnHit")]
+        [HarmonyPatch(typeof(PlayerController), methodName: "OnHit")]
         [HarmonyPostfix]
         public static void PlayerController_OnHit()
         {
+            SimulationManager.Pause();
+            FollowerManager.Reset();
+            StructureManager.Reset();
+            UIDynamicNotificationCenter.Reset();
+            GameManager.GoG_Initialised = false;
+            TwitchManager.Abort();
             SaveAndLoad.DeleteSaveSlot(SaveAndLoad.SAVE_SLOT);
             MMTransition.Play(MMTransition.TransitionType.ChangeSceneAutoResume, MMTransition.Effect.BlackFade, "Main Menu", 0.5f, "", null);
         }
